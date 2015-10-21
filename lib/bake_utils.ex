a3a7f6a9a55fdf002f858b4b4adf72934dbe138d
Justin Schneck <jschneck@mac.com>
2015-10-21 14:36:24 -0400

    Added daemon stuff

--- lib/bake_utils.ex
+++ lib/bake_utils.ex
@@ -1,6 +1,7 @@
 defmodule BakeUtils do
   @working_dir File.cwd! <> "/_bake"
   @bake_home "~/.bake"
+  @daemon_port 10001
 
   def create_working_dir do
     File.mkdir @working_dir
@@ -20,5 +21,22 @@ defmodule BakeUtils do
 
   def oven_dir do
     bake_home <> "/oven"
+  end
+
+  def daemon_running? do
+    case File.read(BakeUtils.daemon_pid) do
+      {:ok, pid} ->
+        :os.cmd(String.to_char_list("kill -0 #{pid}")) == []
+      _ -> false
+    end
+  end
+
+  def daemon_port do
+    System.get_env("BAKE_HOME") || @daemon_port
+  end
+
+  def host_arch do
+    {arch, 0} = System.cmd("uname", ["-m"])
+    arch |> String.strip
   end
 end

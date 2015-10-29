9c73a1430226ed49001cec08550d6ed1bb5cb088
Justin Schneck <jschneck@mac.com>
2015-10-29 16:59:37 -0400

    moved common Cli config blocks into utils

--- lib/bake_utils.ex
+++ lib/bake_utils.ex
@@ -3,6 +3,8 @@ defmodule BakeUtils do
   @bake_home "~/.bake"
   @daemon_port 10001
 
+  require Logger
+
   def create_working_dir do
     File.mkdir @working_dir
   end
@@ -23,6 +25,15 @@ defmodule BakeUtils do
     bake_home <> "/oven"
   end
 
+  def auth_info(config \\ BakeUtils.Cli.Config.read) do
+    Logger.debug "Config: #{inspect config}"
+    if key = config[:key] do
+      [key: key]
+    else
+      Bake.Shell.error "No authorized user found. Run 'bake user auth'"
+    end
+  end
+
   def daemon_running? do
     case File.read(BakeUtils.daemon_pid) do
       {:ok, pid} ->
@@ -38,5 +49,30 @@ defmodule BakeUtils do
   def host_arch do
     {arch, 0} = System.cmd("uname", ["-m"])
     arch |> String.strip
+  end
+
+  def host_platform do
+    {arch, 0} = System.cmd("uname", ["-m"])
+    arch |> String.strip
+  end
+
+  defp platform(<<"Darwin", _tail :: binary>>) do
+    {ver, 0} = System.cmd("sw_vers", ["-productVersion"])
+    ver =
+      ver
+      |> String.strip
+     "Mac OS #{ver}"
+  end
+
+  defp platform(_), do: :unknown
+
+  def local_user(config) do
+    case Keyword.fetch(config, :username) do
+      {:ok, username} ->
+        username
+      :error ->
+        raise Bake.Error, message: "No user authorised on the local machine. Run `bake user auth` " <>
+                  "or create a new user with `bake user register`"
+    end
   end
 end

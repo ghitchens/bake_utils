0e5ec71b87d7dbb54d39014b9653042532516753
Justin Schneck <jschneck@mac.com>
2016-01-05 09:53:51 -0500

    bump release

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule BakeUtils.Mixfile do
 
   def project do
     [app: :bake_utils,
-     version: "0.0.1",
+     version: "0.0.2-dev",
      elixir: "~> 1.1",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
